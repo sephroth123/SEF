8bfb5d64a44749fa13abb812a9265bdfc3629daf
909858512@qq.com <909858512@qq.com>
2020-08-30 20:00:30 +0800

    create_basic_class

---
+++ src/attribute/RoleFramework.java
@@ -0,0 +1,33 @@
+package attribute;
+
+public class RoleFramework {
+
+	private String id;
+	private String r_id;// role_id
+	private String f_id;// framwork_id
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getR_id() {
+		return r_id;
+	}
+
+	public void setR_id(String r_id) {
+		this.r_id = r_id;
+	}
+
+	public String getF_id() {
+		return f_id;
+	}
+
+	public void setF_id(String f_id) {
+		this.f_id = f_id;
+	}
+
+}
