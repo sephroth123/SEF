1938a9354993dbd9420ab72715bb70be53ba4109
sephroth123 <43159552+sephroth123@users.noreply.github.com>
2020-10-09 12:29:46 +0800

    Add files via upload

---
+++ src/client/exception/Invalid_exception.java
@@ -0,0 +1,22 @@
+package project;
+
+
+
+class Invalid_exception extends Exception { 
+    Invalid_exception(String s) {
+        super(s);
+    }
+}
+
+
+class InvalidInputLength_exception extends Exception { 
+    InvalidInputLength_exception(String s) {
+        super(s);
+    }
+}
+
+class InvalidInputLetter_exception extends Exception { 
+    InvalidInputLetter_exception(String s) {
+        super(s);
+    }
+}
